79e5b63afa5a3e6f00288f6f1ed0b5337fb425cd
janisgeegee@gmail.com <janisgeegee@gmail.com>
2024-09-19 16:04:53 +0800

    Added configure and config struct to help prepare using concurrency

---
+++ configure.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+	"sync"
+)
+
+type config struct {
+	pages              map[string]int
+	baseURL            *url.URL
+	mu                 *sync.Mutex
+	concurrencyControl chan struct{}
+	wg                 *sync.WaitGroup
+}
+
+func (cfg *config) configure(rawbaseURL string, maxConcurrency int) (*config, error) {
+	// Initialize pages
+	// Parse and set baseURL
+	baseURL, err := url.Parse(rawbaseURL)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't parse base URL: %v", err)
+	}
+
+	return &config{
+		pages:              make(map[string]int),
+		baseURL:            baseURL,
+		mu:                 &sync.Mutex{},
+		concurrencyControl: make(chan struct{}, maxConcurrency),
+		wg:                 &sync.WaitGroup{},
+	}, nil
+
+}
+
+func (cfg *config) addPageVisit(normalized string) (isFirst bool) {
+	cfg.mu.Lock()
+	defer cfg.mu.Unlock()
+
+	_, visited := cfg.pages[normalized]
+	if visited {
+		cfg.pages[normalized]++
+		return false
+	}
+	cfg.pages[normalized] = 1
+	return true
+}
